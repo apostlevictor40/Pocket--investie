f0e8297eb27b9de493c5a0ee63b297d7f722d4cd
apostlevictor40 <apostlevictor40@gmail.com>
2025-07-30 09:14:52 +0100

    Create email.ts

---
+++ lib/email.ts
@@ -0,0 +1,15 @@
+import { Resend } from "resend"
+const resend = new Resend(process.env.RESEND_API_KEY!)
+
+export async function sendEmail(to: string, subject: string, html: string) {
+  try {
+    await resend.emails.send({
+      from: process.env.EMAIL_FROM!,
+      to,
+      subject,
+      html,
+    })
+  } catch {
+    // logging omitted
+  }
+}
